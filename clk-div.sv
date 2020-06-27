d6d9861bedffc3b5375245554c13cff8df396b8c
Michael LeMay <m@lemays.org>
2020-06-27 12:46:48 -0700

    fix clock divider

--- clk-div.sv
+++ clk-div.sv
@@ -1,6 +1,8 @@
 // Copyright (C) 2019 Michael LeMay
 // SPDX-License-Identifier: MIT
 
+// Divide a clock by a specified factor.
+// Only even factors are precisely supported.
 module clk_div(clk_out, clk_in);
     output clk_out;
     input clk_in;
@@ -9,8 +11,14 @@ module clk_div(clk_out, clk_in);
 
     integer ctr = 1;
 
+    initial begin
+        clk_out = 0;
+    end
+
     always @(posedge clk_in) begin
-        if (ctr == DIV_FACTOR) begin
+        // halve factor to account for the fact that triggering only occurs on
+        // posedge of input clock:
+        if (ctr == DIV_FACTOR/2) begin
             clk_out <= ~clk_out;
             ctr <= 1;
         end else begin
